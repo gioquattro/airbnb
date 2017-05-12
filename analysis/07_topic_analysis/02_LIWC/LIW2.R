25993b2f8628418b1b78bb536176a41a7b0e49ca
gioquattro <gioquattro@gmail.com>
2017-05-12 10:25:15 +0100

    new method to extract words
    Changes:
    
    - 07_topic_analysis folder has been tidies up
    - Changed the method to derive words (the corpus is not composed by 100
    random reviews). The algorithm consider 1k random corpus.
    - New method has been compared against the old method. Results: there
    is consistency and more granularity (see plot idf_methodComparison.pdf )

---
+++ analysis/07_topic_analysis/02_LIWC/LIW2.R
@@ -0,0 +1,93 @@
+library(RMySQL)
+library(data.table)
+
+# 
+# loading datast
+# 
+
+mydb = dbConnect(MySQL(), user='root', password='', dbname='airbnb', host='127.0.0.1')
+# mydb = dbConnect(MySQL(), user='root', password='1234567890', dbname='airbnb', host='127.0.0.1')
+
+dbListTables(mydb)
+
+rs = dbSendQuery(mydb, "select * from guest")
+guest = fetch(rs, n=-1)
+
+rs = dbSendQuery(mydb, "select * from listing")
+listing = fetch(rs, n=-1)
+
+rs = dbSendQuery(mydb, "select * from review")
+review = fetch(rs, n=-1)
+
+Encoding(review$comments) <- "latin1"
+Encoding(review$created_at) <- "latin1"
+
+guest = data.table(guest)
+listing = data.table(listing)
+review = data.table(review)
+
+
+
+# 
+# reviews only for guests
+# 
+
+review_g = review[author_id %in% guest$id]
+review_g$year = as.numeric( substr(review_g$created_at, (nchar(review_g$created_at)-5), (nchar(review_g$created_at)-1) ) )
+
+table(review_g$year)
+# 2010 2011 2012 2013 2014 2015 2016 2017 
+#    4   16   72   65   83  131  275  209 
+
+review_g$inferredLanguage = textcat(review_g$comments)
+review_g$nwords = sapply(gregexpr("\\W+", review_g$comments), length) + 1
+review_g$nchars = nchar(review_g$comments)
+
+ggplot(review_g, aes(inferredLanguage)) + geom_bar() + facet_grid(year ~ .)
+
+review_g = review_g[!is.na(year) & inferredLanguage=='english']
+
+summary(review_g$nwords)
+ggplot(review_g, aes(x=factor(year), y=nwords)) + geom_boxplot() + geom_point(alpha=0.1, col=3) + ylim(0,250)
+
+
+
+
+# 
+# text analysis
+# 
+
+library("quanteda")
+mfdict <- dictionary(file = "LIWC2007_English080730.dic", format = "LIWC")
+doc_term_matrix <- dfm(review_g$comments, stem = FALSE, dictionary = mfdict)
+doc_term_matrix_norm = doc_term_matrix / review_g$nwords
+
+doc_term_df = data.table( data.frame(doc_term_matrix) ) 
+doc_term_df_norm = data.table( data.frame(doc_term_matrix_norm) )
+
+doc_term_df_norm_join = cbind(review_g[, c('year'), with=F], doc_term_df_norm)
+
+
+# 
+# analysis by year
+# 
+
+attach(doc_term_df_norm_join)
+aggdata <-aggregate(doc_term_df_norm_join, by=list(year), FUN=mean, na.rm=TRUE)
+detach(doc_term_df_norm_join)
+aggdata$Group.1 = NULL
+
+
+
+
+# 
+# visualise
+# 
+
+library(reshape)
+meltdata <- melt(aggdata, id=c("year"))
+names(meltdata)
+
+library(ggplot2)
+ggplot(meltdata, aes(x=variable, y=value)) + geom_bar(stat="identity", col=1, fill=3) +
+  facet_grid(year ~ .) + theme(axis.text.x = element_text(angle = 90, hjust=1, vjust=0.5))
